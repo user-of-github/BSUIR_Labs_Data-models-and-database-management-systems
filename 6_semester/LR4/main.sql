f796a604ed8a7e7eb5351d248afb9bb9abed6656
Nikita <nikitosminsk100@gmail.com>
2023-04-29 15:01:45 +0300

    feat: add full JSON doc with queries examples for testing

--- 6_semester/LR4/main.sql
+++ 6_semester/LR4/main.sql
@@ -3,9 +3,26 @@
 DECLARE
     query VARCHAR2(4000) := '
 {
-    "queryType": "CREATE",
-    "tableName": "table1",
-    "fields": ["id NUMBER NOT NULL PRIMARY KEY", "col1 VARCHAR2(100) NOT NULL", "col2 VARCHAR2(100) NOT NULL"]
+    "queryType": "SELECT",
+    "columnsNames": ["table1.id, table1.col1, table1.col2"],
+    "tablesNames": ["table1" ],
+    "where": [
+        {
+            "usualCondition": "col1 < 10",
+            "separator": "AND"
+        },
+        {
+            "usualCondition": "1 = 1",
+            "separator": "AND"
+        },
+        {
+            "in": {
+                "columnName": "col1",
+                "subquerySelect": {"queryType": "SELECT","columnsNames": ["col1"],"tablesNames": ["table2"]}
+            },
+            "separator": "OR"
+        }
+    ]
 }
 ';
 
@@ -14,7 +31,7 @@ DECLARE
     result_response_query_string VARCHAR2(4096) := NULL;
     response_for_select_query_cursor SYS_REFCURSOR := null;
 
-    -- SAME COLUMNS AS IN QUERY !!! (TO DEMONSTRATE)
+    -- (2) SAME COLUMNS AS IN QUERY !!! (TO DEMONSTRATE) // enable only when testing select
     id_temp table1.id%TYPE := NULL;
     col1_temp table1.col1%TYPE := NULL;
     col2_temp table1.col2%TYPE := NULL;
@@ -22,21 +39,21 @@ DECLARE
 BEGIN
     parsed_document := JSON_OBJECT_T.parse(query); 
 
-    -- TEST JUST PRINTING USUAL QUERY (AND EXECUTING WITHOUT OUTPUT)
-    result_response_query_string := build_generic_query(parsed_document);
-    DBMS_OUTPUT.PUT_LINE('[main] ' || result_response_query_string);
+    -- (1) TEST JUST PRINTING USUAL QUERY (AND EXECUTING WITHOUT OUTPUT)
+    --result_response_query_string := build_generic_query(parsed_document);
+    --DBMS_OUTPUT.PUT_LINE('[main] ' || result_response_query_string);
+    --EXECUTE IMMEDIATE result_response_query_string;
 
-
-    -- TEST SELECT CURSOR:  
-    --response_for_select_query_cursor := get_cursor_from_select_query(parsed_document);
-    --DBMS_OUTPUT.PUT_LINE(' ');
-    --DBMS_OUTPUT.PUT_LINE('[main] PRINTING VALUES FROM CURSOR FROM SELECT QUERY'); -- IMPORTANT TO FETCH THE SAME VARS !!!
-    --LOOP
-    --    FETCH response_for_select_query_cursor INTO id_temp, col1_temp, col2_temp, name_temp;
-    --    EXIT WHEN response_for_select_query_cursor%NOTFOUND;
-    --    DBMS_OUTPUT.PUT_LINE('  ROW:  ( ' || id_temp || ' , ' || col1_temp || ' , ' || col2_temp || ' , ' || name_temp || ' )');
-    --END LOOP;
-    --CLOSE response_for_select_query_cursor;
+    -- (2) TEST SELECT CURSOR:  
+    response_for_select_query_cursor := get_cursor_from_select_query(parsed_document);
+    DBMS_OUTPUT.PUT_LINE(' ');
+    DBMS_OUTPUT.PUT_LINE('[main] PRINTING VALUES FROM CURSOR FROM SELECT QUERY'); -- IMPORTANT TO FETCH THE SAME VARS !!!
+    LOOP
+        FETCH response_for_select_query_cursor INTO id_temp, col1_temp, col2_temp;
+        EXIT WHEN response_for_select_query_cursor%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('  ROW:  ( ' || id_temp || ' , ' || col1_temp || ' , ' || col2_temp || ' , ' || name_temp || ' )');
+    END LOOP;
+    CLOSE response_for_select_query_cursor;
 END;
 
 /
